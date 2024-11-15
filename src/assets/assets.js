6440012560cedb043091bf2a8e66a03cf3a73a5c
Nsuku Mathebula <breakfastcaf@gmail.com>
2024-11-15 11:58:40 +0200

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,223 @@
+import bell_icon from './bell_icon.png'
+import arrow_icon from './arrow_icon.png'
+
+import home_icon from './home_icon.png'
+import global_pic from './global_pic.jpg'
+import south_icon from './south_icon.jpg'
+import hip_hop from './hip_hop.jpeg'
+import rnb_mix from './rnb_mix.jpeg'
+import gospel_mix from './gospel_mix.jpeg'
+import amapiano_mix from './amapiano_mix.jpeg'
+import search_icon from './search_icon.png'
+import stack_icon from './stack_icon.png'
+import plus_icon from './plus_icon.png'
+import song1_pic from './song1_pic.jpg'
+import song2_pic from './song2_pic.jpg'
+import song3_pic from './song3_pic.jpeg'
+import song4_pic from './song4_pic.jpg'
+import song5_pic from './song5_pic.jpg'
+import song6_pic from './song6_pic.jpg'
+import song7_pic from './song7_pic.jpg'
+import song8_pic from './song8_pic.jpg'
+import shuffle_icon from './shuffle_icon.png'
+import next_icon from './next_icon.png'
+import play_icon from './play_icon.png'
+import prev_icon from './prev_icon.png'
+import repeat_icon from './repeat_icon.png'
+import mic_icon from './mic_icon.png'
+import queue_icon from './queue_icon.png'
+import speaker_icon from './speaker_icon.png'
+import volume_icon from './volume_icon.png'
+import mini_player_icon from './mini_player_icon.png'
+import zoom_icon from './zoom_icon.png'
+import arrow_icon_left from './arrow_icon_left.png'
+import arrow_iocn_right from './arrow_icon_right.png'
+import spotify_icon from './spotify_icon.png'
+import clock_icon from './clock_icon.png'
+import pause_icon from './pause_icon.png'
+import song1 from './song1.mp3'
+import song2 from './song2.mp3'
+import song3 from './song3.mp3'
+import song4 from './song4.mp3'
+import song5 from './song5.mp3'
+import song6 from './song6.mp3'
+import song7 from './song7.mp3'
+import song8 from './song8.mp3'
+
+
+
+
+
+
+export const assets = {
+    bell_icon,
+    arrow_icon,
+    clock_icon,
+    home_icon,
+    global_pic,
+    south_icon,
+    hip_hop,
+    rnb_mix,
+    gospel_mix,
+    amapiano_mix,
+    search_icon,
+    stack_icon,
+    plus_icon,
+    song1_pic,
+    song2_pic,
+    song3_pic,
+    song4_pic,
+    song5_pic,
+    song6_pic,
+    song7_pic,
+    song8_pic,
+    shuffle_icon,
+    prev_icon,
+    next_icon,
+    play_icon,
+    repeat_icon,
+    mic_icon,
+    queue_icon,
+    speaker_icon,
+    volume_icon,
+    mini_player_icon,
+    zoom_icon,
+    arrow_icon_left,
+    arrow_iocn_right,
+    spotify_icon,
+    pause_icon,
+ 
+    song1,
+    song2,
+    song3,
+    song4,
+    song5,
+    song6,
+    song7,
+    song8,
+   
+}
+
+export const albumsData = [
+    {
+        id:0,
+        name: "Top 50 Global",
+        image: global_pic,
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#51414F"
+    },
+    {
+        id:1,
+        name: "Top 50 South Africa",
+        image: south_icon,
+        bgColor:"#22543d"
+    },
+
+    {
+        id:2,
+        name: "Top hip hop songs",
+        image: hip_hop,
+        desc: "Your daily hip hop mix",
+        bgColor: "#FF0000"
+    },
+
+    {
+        id:3,
+        name: "Top RNB songs",
+        image: rnb_mix,
+        desc: "Your daily RNB mix",
+        bgColor: "#808080"
+    },
+{
+    id:4,
+    name: "Top Amapiano songs",
+    image: amapiano_mix,
+    desc: "Your daily amapiano mix",
+    bgColor:"#964B00"
+}
+
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Dilemma",
+        file: song1,
+        image: song1_pic,
+        desc:"Feel the romance with these tunes",
+        duration:"4.47"
+
+    },
+
+    {
+        id:1,
+        name: "Pony",
+        file: song2,
+        image: song2_pic,
+        desc: "Get freaky with these tunes",
+        duration: "4.10"
+    },
+
+    {
+        id:2,
+        name: "You rock my world",
+        file: song3,
+        image: song3_pic,
+        desc: "Dance to these tunes",
+        duration:"5.37"
+    },
+    
+    {
+        id:3,
+        name: "Biri Marung",
+        file: song4,
+        image: song4_pic,
+        desc: "Dance to these tunes",
+        duration:"7.09"
+    },
+
+    {
+        id:4,
+        name: "Headlines",
+        file: song5,
+        image: song5_pic,
+        desc: "Jam to these tunes",
+        duration:"4.04"
+    },
+
+    {
+        id:5,
+        name: "Sticky",
+        file: song6,
+        image: song6_pic,
+        desc: "Dance to these tunes",
+        duration:"4.09"
+    },
+
+    {
+        id:6,
+        name: "No guidance",
+        file: song7,
+        image: song7_pic,
+        desc: "Chill to these tunes",
+        duration:"4.22"
+    },
+
+    {
+        id:7,
+        name: "Imaginary players",
+        file: song8,
+        image: song8_pic,
+        desc: "Bounce to these tunes",
+        duration:"4.46"
+    },
+
+   
+
+
+
+
+]
+export const url = 'http://localhost:5173'
+
+
